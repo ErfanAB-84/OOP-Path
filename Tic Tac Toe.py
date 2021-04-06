f90adb23a73b8fe8971f611e04a1d9ee3f45db19
hoco1 <mohammadrezanorouzi600@gmail.com>
2021-04-06 15:14:49 +0430

    Create game board

---
+++ Tic Tac Toe.py
@@ -0,0 +1,21 @@
+import time
+
+class Game:
+    def __init__(self):
+        self.initialize_game()
+
+    def initialize_game(self):
+        self.current_state = []
+        for i in range(3):
+            self.current_state.append(['.' for c in range(3)])
+
+
+        # Player X always plays first
+        self.player_turn = 'X'
+
+    def draw_current_state(self):
+        for i in range(0, 3):
+            for j in range(0, 3):
+                print('{}|'.format(self.current_state[i][j]), end=" ")
+            print()
+        print()
